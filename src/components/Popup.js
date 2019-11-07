b77663f33e9050f1d0262a48271a39e3b5f75aed
Erik Pehrers <erik.pehrers@jetshop.se>
2019-11-07 16:09:53 +0100

    minor fixes...

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -17,13 +17,19 @@ const Backdrop = styled.div`
 const PopupContainer = styled(animated.div)`
   position: absolute;
   z-index: 200;
-  left: 50%;
+  width: 450px;
+  max-width: 100%;
+  max-height: 100%;
+  padding: 16px;
   top: 50%;
-  box-shadow: ${theme.boxShadow};
+  left: 50%;
   @media screen and (max-width: 40em) {
-    left: 10%;
-    right: 10%;
+    left: 0%;
+    right: 0%;
     top: 2%;
+    &&& {
+      transform: translate(0%, 0%) !important;
+    }
     &.center {
       top: 50%;
     }
@@ -35,9 +41,9 @@ const Popup = ({ children, id, className }) => {
 
   const transition = useTransition(id === popupId, null, {
     config: { duration: 75, tension: 300 },
-    from: { transform: 'scale(0)', opacity: 0 },
-    enter: { transform: 'scale(1)', opacity: 1 },
-    leave: { transform: 'scale(0)', opacity: 0 }
+    from: { transform: 'translate(-50%, -50%) scale(0)', opacity: 0 },
+    enter: { transform: 'translate(-50%, -50%) scale(1)', opacity: 1 },
+    leave: { transform: 'translate(-50%, -50%) scale(0)', opacity: 0 }
   });
 
   return id === popupId
@@ -52,9 +58,7 @@ const Popup = ({ children, id, className }) => {
               )
             );
           })}
-          {id === popupId && (
-            <Backdrop onClick={hideWithBackdrop ? hidePopup : undefined} />
-          )}
+          {id === popupId && <Backdrop onClick={hidePopup} />}
         </>,
         document.getElementById('modalRoot')
       )
